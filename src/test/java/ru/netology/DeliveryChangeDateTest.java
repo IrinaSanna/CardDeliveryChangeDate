5946e6d266a084b5815c914f3811cd02cc197b8c
Irina D <RabotaYd2021@yandex.ru>
2024-05-26 18:00:20 +0300

    Added Allure Report

--- src/test/java/ru/netology/DeliveryChangeDateTest.java
+++ src/test/java/ru/netology/DeliveryChangeDateTest.java
@@ -2,9 +2,9 @@ package ru.netology;
 
 
 import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -14,6 +14,14 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryChangeDateTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
